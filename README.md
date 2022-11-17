5c38389660cb4088e43a0b902f7dea4beee9d910
Manas Vijaywargiya <manasdevslopes@gmail.com>
2022-11-18 00:02:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# AwesomeNetwork
+
+A description of this package.
---
+++ Sources/AwesomeNetwork.xcframework/ios-arm64/AwesomeNetwork.framework/Headers/AwesomeNetwork.h
@@ -0,0 +1,18 @@
+//
+//  AwesomeNetwork.h
+//  AwesomeNetwork
+//
+//  Created by MANAS VIJAYWARGIYA on 17/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AwesomeNetwork.
+FOUNDATION_EXPORT double AwesomeNetworkVersionNumber;
+
+//! Project version string for AwesomeNetwork.
+FOUNDATION_EXPORT const unsigned char AwesomeNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeNetwork/PublicHeader.h>
+
+
